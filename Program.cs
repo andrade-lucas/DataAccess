2a10241ee42289e2e49f5a177beb9d0e73fa5a68
Lucas Andrade <lucasilva108@gmail.com>
2024-09-21 13:16:28 -0300

    feat: update program

--- Program.cs
+++ Program.cs
@@ -4,20 +4,67 @@ using Microsoft.Data.SqlClient;
 
 const string connectionString = "Server=localhost,1433; Database=Blog;User Id=sa; Password=1q2w3e4r@#$;TrustServerCertificate=True";
 
-using var connection = new SqlConnection(connectionString);
-var repository = new UserRepository(connection);
-var users = repository.Get();
+var connection = new SqlConnection(connectionString);
+connection.Open();
 
-foreach (var user in users)
-    Console.WriteLine(user.Name);
+ReadUser(connection);
+//CreateUser(connection);
+//ReadRoles(connection);
+//ReadTags(connection);
 
+connection.Close();
 
-//var user = new User
-//{
-//    Name = "Batman",
-//    Email = "batman@wayne.com",
-//    PasswordHash = "HASH",
-//    Bio = "Alguma biografia aqui",
-//    Image = "https://",
-//    Slug = "batman"
-//};
+static void ReadUser(SqlConnection connection)
+{
+    var repository = new UserRepository(connection);
+    var items = repository.GetWithRoles();
+
+    foreach (var item in items)
+    {
+        Console.WriteLine($"{item.Name} ({item.Email})");
+        foreach (var role in item.Roles)
+        {
+            Console.WriteLine($"- {role.Name}");
+        }
+    }
+}
+
+static void CreateUser(SqlConnection connection)
+{
+    var user = new User
+    {
+        Name = "Teste",
+        Email = "teste@teste.com",
+        Bio = "aaaaa aaaa aa aa a",
+        Image = "https://",
+        PasswordHash = "HASH",
+        Slug = "teste-01"
+    };
+    //var role = new Role
+    //{
+    //    Name = "Teste",
+    //    Slug = "teste-01"
+    //};
+    //user.Roles.Add(role);
+
+    var repository = new Repository<User>(connection);
+    repository.Create(user);
+}
+
+static void ReadRoles(SqlConnection connection)
+{
+    var repository = new Repository<Role>(connection);
+    var items = repository.GetAll();
+
+    foreach (var item in items)
+        Console.WriteLine("Role: " + item.Name);
+}
+
+static void ReadTags(SqlConnection connection)
+{
+    var repository = new Repository<Tag>(connection);
+    var items = repository.GetAll();
+
+    foreach (var item in items)
+        Console.WriteLine("Tag: " + item.Name);
+}
