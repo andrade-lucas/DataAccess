737fd739ff76b009f8d8817867690916ed0067e9
Lucas Andrade <lucasilva108@gmail.com>
2024-09-21 10:24:39 -0300

    feat: add models

---
+++ Models/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Category Category { get; set; }
+    public User Author { get; set; }
+    public string Sumary { get; set; }
+    public string Body { get; set; }
+    public string Slug { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime LastUpdateDate { get; set; }
+}
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -2,10 +2,11 @@
 
 namespace DataAccess.Models;
 
-[Table("[Category]")]
-public class Category
+[Table("[Tag]")]
+public class Tag
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Slug { get; set; }
+    public List<Post> Posts { get; set; }
 }
