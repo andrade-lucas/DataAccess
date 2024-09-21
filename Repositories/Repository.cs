084413f97917f6b5f9643c0c58d5467068aba56b
Lucas Andrade <lucasilva108@gmail.com>
2024-09-21 13:15:41 -0300

    feat: add generic repository

---
+++ Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Dapper.Contrib.Extensions;
+using DataAccess.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess.Repositories;
+
+public class Repository<T> where T : class
+{
+    private readonly SqlConnection _connection;
+
+    public Repository(SqlConnection connection)
+        => _connection = connection;
+
+    public IEnumerable<T> GetAll()
+        => _connection.GetAll<T>();
+
+    public T GetById(int modelId) 
+        => _connection.Get<T>(modelId);
+
+    public void Create(T model)
+        => _connection.Insert<T>(model);
+
+    public void Update(T model)
+        => _connection.Update<T>(model);
+
+    public void Delete(int modelId)
+    {
+        var model = _connection.Get<T>(modelId);
+        _connection.Delete<T>(model);
+    }
+
+    public void Delete(T model)
+        => _connection.Delete<T>(model);
+}
