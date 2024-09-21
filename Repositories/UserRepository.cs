45809e5cb226de68a2a696bcf6acfb8097577f54
Lucas Andrade <lucasilva108@gmail.com>
2024-09-21 09:36:29 -0300

    feat: create user repository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper.Contrib.Extensions;
+using DataAccess.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccess.Repositories;
+
+public class UserRepository
+{
+    private readonly SqlConnection _connection;
+
+    public UserRepository(SqlConnection connection)
+        => _connection = connection;
+
+    public IEnumerable<User> Get() => _connection.GetAll<User>();
+
+    public User GetById(int userId) => _connection.Get<User>(userId);
+
+    public void Create(User user) => _connection.Insert<User>(user);
+
+    public void Update(User user) => _connection.Update<User>(user);
+
+    public void Delete(int userId)
+    {
+        var user = _connection.Get<User>(4);
+        _connection.Delete<User>(user);
+    }
+}
